55724cba8d4e323546ee2d7714976803b5e2d450
appelinta3222ant-stack <appelinta3222ant@student.fatima.edu.ph>
2026-05-02 19:46:40 +0800

    changes

--- script.js
+++ script.js
@@ -405,3 +405,20 @@ document.addEventListener("DOMContentLoaded", () => {
   updateCartCount();
   displayCart();
 });
+
+// ================= carousel =================
+let slides = document.querySelectorAll(".slide");
+let current = 0;
+
+function showSlide(index) {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+function nextSlide() {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}
+
+// auto change every 3 seconds
+setInterval(nextSlide, 3000);
